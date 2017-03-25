f7ed50ac78d2607994ecf6a7eca6c8f41033ebb9
Moises <moisesapaza07@gmail.com>
2017-03-25 04:13:07 -0300

    implementing navigation view

--- app/src/main/java/moises/com/appcoer/ui/LoginActivity.java
+++ app/src/main/java/moises/com/appcoer/ui/LoginActivity.java
@@ -1,7 +1,10 @@
 package moises.com.appcoer.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import moises.com.appcoer.R;
 
@@ -11,5 +14,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button bLogin = (Button)findViewById(R.id.b_login);
+        bLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
     }
 }
