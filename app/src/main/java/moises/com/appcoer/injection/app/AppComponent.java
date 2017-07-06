855cb3b37c12b9673303d026f5c9de9ea395676d
Moises <moisesapaza07@gmail.com>
2017-07-06 02:01:29 -0300

    added config and dependencies

---
+++ app/src/main/java/moises/com/appcoer/injection/app/AppComponent.java
@@ -0,0 +1,8 @@
+package moises.com.appcoer.injection.app;
+
+/**
+ * Created by moises on 7/6/17.
+ */
+
+public class AppComponent {
+}
---
+++ app/src/main/java/moises/com/appcoer/injection/login/LoginActivityComponent.java
@@ -0,0 +1,8 @@
+package moises.com.appcoer.injection.login;
+
+/**
+ * Created by moises on 7/6/17.
+ */
+
+public class LoginActivityComponent {
+}
